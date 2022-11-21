3316396698ca082be82d80dd848d658ef7136065
Eswarbaskar <eswarbilla89@gmail.com>
2022-11-21 18:15:51 +0530

    align

--- src/pages/Colorbox.js
+++ src/pages/Colorbox.js
@@ -11,7 +11,7 @@ function Colorbox({color}) {
   return (
     <div className="container m-0">
       <div className="row justify-content-center">
-        <div className="col-md-10">
+        <div className="col-lg-10">
         
         <div style={styles} className="border border-2"></div>
 
--- src/pages/Colourgame.js
+++ src/pages/Colourgame.js
@@ -3,34 +3,42 @@ import Colorbox from './Colorbox';
 
 function Colourgame() {
     const [color, setColor] = useState("");
-    const [colorlist,setcolorlist]=useState([])
+    const [colorlist, setcolorlist] = useState([])
     const styles = {
         backgroundColor: color,
         fontSize: '35px'
     };
     return (
-        <div>
-            <div className='text-center p-4 m-4 add-color'>
-            <input
-                onChange={(event) => setColor(event.target.value)}
-                type="text"
-                style={styles}
+        <div className="container text-center m-4 p-4">
+            <div className="row">
+                <div className="col-lg-12">
+                    <div>
+                        <div className='add-color '>
+                            <input
+                                onChange={(event) => setColor(event.target.value)}
+                                type="text"
+                                style={styles}
 
-                value={color}
-                placeholder="Enter a colour"
-            />
-            <button class="btn btn-primary" onClick={()=>setcolorlist([...colorlist,color])} >Addcolor</button>
-           </div>    
-            {
-                colorlist.map((clr)=>{
-                    return(
-                    <Colorbox color={clr}/>
-                    )
-                })
-            }
-            
+                                value={color}
+                                placeholder="Enter a colour"
+                            />
+                            <button class="btn btn-primary" onClick={() => setcolorlist([...colorlist, color])} >Addcolor</button>
+                        </div>
+                        {
+                            colorlist.map((clr) => {
+                                return (
+                                    <Colorbox color={clr} />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+
         </div>
-       
+
+
+
     )
 }
 
